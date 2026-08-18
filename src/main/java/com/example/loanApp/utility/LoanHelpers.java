75653c8a04280e85bd1134a338389e5685c4d384
alfaxad1 <alfaxadder@gmail.com>
2026-08-18 11:39:04 +0300

    view loan issue

--- src/main/java/com/example/loanApp/utility/LoanHelpers.java
+++ src/main/java/com/example/loanApp/utility/LoanHelpers.java
@@ -9,12 +9,16 @@ import org.springframework.stereotype.Service;
 public class LoanHelpers {
 
     public String getLoanStatusDescription(LoanStatus status) {
+        if (status == null) {
+            return "Unknown Status";
+        }
         return switch (status) {
             case active -> "Active";
             case partially_paid -> "Partial";
             case paid -> "Paid";
             case defaulted -> "Defaulted";
             case pending_disbursement -> "Pending Disbursement";
+            case rolled_over -> "Rolled Over";
             default -> "Unknown Status";
         };
     }
